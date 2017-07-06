24cd848a03d192ac3f9d1e991a6ecae03a6150be
Ayush Garg <ayushgargdroid@gmail.com>
2017-07-06 21:03:25 +0530

    Removing unnecessary resources.

--- AttendanceProjectRpi/index.js
+++ AttendanceProjectRpi/index.js
@@ -26,7 +26,7 @@
 
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
-const url = require('url')
+var url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
