8f0152d6a670ef4d149d72156ff5e29ee9a1ed8a
Ayush Garg <ayushgargdroid@gmail.com>
2017-07-07 20:48:40 +0530

    Removing Web part for deploy.

---
+++ AttendanceProjectRpi/public/delete.js
@@ -0,0 +1,57 @@
+const _ = require('lodash');
+const SerialPort = require('serialport');
+
+var port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600,
+    autoOpen: false,
+    parser: SerialPort.parsers.readline('\r\n')
+});
+var openConn = () => {
+    port.open((err) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log('Port opened');
+    });
+}
+var sendData = (data) => {
+    if(port.isOpen()){
+        console.log('Port checked for opening');
+        console.log(data);
+        port.write(data,(err) => {
+            if(err){
+                return console.log(err);
+            }
+            console.log(data + ' sent.');
+        });
+    }
+}
+
+var collector = '';
+var emp = ['Please select something','Ayush','Tanish','Sharma','Gupta'];
+
+var populateSelect = () => {
+    for(var i=0;i<emp.length;i++){
+        collector = collector + `<option value="${emp[i]}">${emp[i]}</option>`
+    }
+    $('#sel1').html(collector);
+    collector = '';
+}
+$(document).ready(() => {
+    populateSelect();
+});
+
+$(document).on('change','#sel1',(e) => {
+    var t = $('#sel1').val();
+    setTimeout(()=>{
+        sendData(`{id}c`);
+    },1500);
+    $('#myModalLabel').html(t);
+    $('#myModal').modal();
+    $('#close-button').click(() => {
+        _.remove(emp,(name) => {
+            return name === t;
+        });
+        populateSelect();
+    })
+})
--- AttendanceProjectWeb/views/public/js/current-status.js
+++
@@ -1,2 +0,0 @@
-$('.collapsible').collapsible();
-$(".button-collapse").sideNav();
