1e23f93fabd0c40f44119a9969ec0e672321dc89
Ayush Garg <ayushgargdroid@gmail.com>
2017-07-07 11:17:47 +0530

    Retrying SerialPort on RPi via NodeJS.

--- AttendanceProjectRpi/public/testSerial.js
+++ AttendanceProjectRpi/public/testSerial.js
@@ -1,17 +1,20 @@
 var SerialPort = require('serialport');
 
-var port = new SerialPort('ACM0', {
+var port = new SerialPort('/dev/ttyACM0', {
     baudRate: 9600,
     parser: SerialPort.parsers.readline('\n')
 });
 
 port.on('open', () => {
-    port.write('a',(err) => {
-        if(err){
-            return console.log(err);
-        }
-        console.log('Message written');
-    });;
+    setTimeout(() => {
+        port.write('a',(err) => {
+            if(err){
+                return console.log(err);
+            }
+            console.log('Message written');
+        });
+    },2000);
+    
 });
 
 port.on('data' ,(data) => {
