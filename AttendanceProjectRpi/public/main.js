9ee50c018e62808690bd13d6b18d1c5c241e9800
Ayush Garg <ayushgargdroid@gmail.com>
2017-07-09 00:28:26 +0530

    Removing bugs from index.html

--- AttendanceProjectRpi/public/main.js
+++ AttendanceProjectRpi/public/main.js
@@ -199,11 +199,17 @@ function updateTime(){
 
 $("#login-pin").swipe({
     tap:function(event,target){
-        if(port.isOpen()){
-            sendData('d');
-            port.close();
+        if(loginAttemptUp==true){
+            if(port.isOpen()){
+                sendData('d');
+                port.close();
+            }
         }
         if(loginAttemptUp==false){
+            if(port.isOpen()){
+                sendData('d');
+                port.close();
+            }
             $("#keyboard-layout").removeClass('hidden');
             slideUpObj.start = 480;
             slideUpObj.end = 310;
